d548dcbbe18a8bf3c50e2918676684bf061ad1ea
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 17:46:15 -0300

    media de 2 valores

---
+++ exercicios/ex007.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia as duas notas de um aluno, calcule e mostre sua media
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+media = (nota1 + nota2)/2
+
+print(f'A media desse aluno é {media}')
