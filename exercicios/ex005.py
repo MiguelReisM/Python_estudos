93ad15247d08b27158edfd16bd704b76576315dd
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 17:36:13 -0300

    antecessor e sucessor de um numero

---
+++ exercicios/ex005.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia um numero inteiro e mostre na tela seu sucessor e antecessor
+
+num1 = int(input('Digite um numero: '))
+print(f'O antecessor de {num1} é {num1 - 1} e o sucessor é {num1 + 1}.')
