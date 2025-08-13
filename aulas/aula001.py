e7671243fb5c2da148aac90ed17b99aa374f4a8f
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-12 21:59:35 -0300

    relembrando

--- aulas/aula001.py
+++ aulas/aula001.py
@@ -2,3 +2,13 @@
 Construindo uma base em Python
 Aula 001 - Introdução ao Python
 '''
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+peso = float(input('Digite seu peso: '))
+
+num1 = int(input('digite um numero: '))
+num2 = int(input('digite um numero: '))
+resultado = num1 + num2
+
+print(f'Meu nome é {nome} tenho {idade} anos e peso {peso}Kg.')
+print(f'A soma de {num1} + {num2} é = {resultado}')
