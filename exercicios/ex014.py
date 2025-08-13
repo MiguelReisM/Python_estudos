60b6f7e6110684af9812bb7963cc7e6934af66fd
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 20:23:04 -0300

    conversor de celsius para fahrenheit

---
+++ exercicios/ex014.py
@@ -0,0 +1,5 @@
+#Crie um conversor de graus C para F
+temperatura_c = float(input('Digite a temperatura: '))
+temperatura_f = ((9 * temperatura_c) / 5) + 32
+
+print(f'A temperatura de {temperatura_c}C corresponde a {temperatura_f}F')
