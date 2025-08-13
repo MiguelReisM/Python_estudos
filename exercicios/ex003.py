854361632767f19efbe764fdeacb14166e78f3e9
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 16:00:29 -0300

    soma de 2 numeros

---
+++ exercicios/ex003.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia dois numeros e mostre a soma entre eles
+
+num1 = int(input('Digite o primeiro numero da soma: '))
+num2 = int(input('Digite o segundo numero da soma: '))
+resultado = num1 + num2
+
+print(f'A soma de {num1} + {num2} é = {resultado}')
