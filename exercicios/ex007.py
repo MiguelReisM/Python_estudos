b8bed18549f99aed3a7ad53d7657dd91ba243e6b
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 18:34:19 -0300

    ajustes

--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -1,6 +1,6 @@
 #Crie um programa que leia as duas notas de um aluno, calcule e mostre sua media
-nota1 = int(input('Digite a primeira nota: '))
-nota2 = int(input('Digite a segunda nota: '))
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2)/2
 
-print(f'A media desse aluno é {media}')
+print(f'A media desse aluno é {media:.2f}')
