d10f4677994e33eb0311015de52d4960c6a2a899
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 17:51:14 -0300

    conversao de metros para cm e mm

---
+++ exercicios/ex008.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
+medida = int(input('Digite a metragem: '))
+
+print(f'Foram digitados {medida}M, que são o mesmo que {medida * 100}cm e tantos milimetros {medida * 1000 }mm')
