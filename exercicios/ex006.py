2104999c6f414d089debf9cfc38e704c99348d7c
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 17:40:49 -0300

    operacoes matematicas

---
+++ exercicios/ex006.py
@@ -0,0 +1,3 @@
+#Crie um algoritimo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
+num1 = int(input('Digite seu numero: '))
+print(f'O numero digitado é {num1}, sendo o seu dobro = {num1 * 2}, seu triplo = {num1 * 3} e a raiz quadrada {num1 ** (1/2):.0f}')
