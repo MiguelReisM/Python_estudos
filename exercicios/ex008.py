3cd766b9e920a600ca60c251e23715710b01e15c
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 18:41:58 -0300

    ajuste em notacao cientifica

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,4 +1,4 @@
 #Crie um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
 medida = float(input('Digite a metragem: '))
 
-print(f'Foram digitados {medida:.2f}M, que são o mesmo que {medida * 100:.2f}cm e tantos milimetros {medida * 1000 :.2f}mm')
+print(f'Foram digitados {medida:.2f}M, que são o mesmo que {medida * 100:.0f}cm e tantos milimetros {medida * 1000 :.0f}mm')
