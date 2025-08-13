d7c5f18e69d3587c5d2f6fe3d769350c76a5c7cd
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-13 18:17:05 -0300

    aplicando 5% de desconto

--- exercicios/ex012.py
+++ exercicios/ex012.py
@@ -1,1 +1,5 @@
-#Crie um algoritmo
+#Crie um algoritmo que leia o preco de um produto e mostre seu novo preco, com 5% de desconto.
+preco = float(input('Digite o valor do produto (R$): '))
+desconto = preco - (preco * 0.05)
+
+print(f'O novo valor desse produto com 5% de desconto é: R${desconto:.2f}')
