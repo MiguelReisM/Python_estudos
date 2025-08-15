41d8330d49d2e9bf4b40e86d189cf24a87873350
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-14 23:02:21 -0300

    melhorias

--- exercicios/ex027.py
+++ exercicios/ex027.py
@@ -2,9 +2,10 @@
 # ex: Ana Maria de Souza
 # Primeiro = Ana
 # Ultimo = Souza
-nome = str(input('Digite seu nome completo: ')).lower().title().strip().split()
-primeiro = nome[0]
-ultimo = nome[-1]
+nome = str(input('Digite seu nome completo: ')).lower().title().strip()
+contagem = nome.split()
+primeiro = contagem[0]
+ultimo = (contagem[len(contagem)-1])
 
 print('Analisando nome...')
 print(f'Seu primeiro nome é: {primeiro}')
