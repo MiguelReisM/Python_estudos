9c7716266cc642d2a0f59596136c7ded73732f71
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-14 22:48:31 -0300

    encontrando letras

--- exercicios/ex026.py
+++ exercicios/ex026.py
@@ -2,3 +2,11 @@
 # Quantas vezes aparece a letra "A"
 # Em que posicao ela aparece a primeira vez
 # Em que posicao ela aparece a ultima vez.
+frase = str(input('Digite uma frase: ')).strip().lower()
+aparecer = frase.count('A')
+primeira_vez = frase.find('A')
+ultima_vez = frase.rfind('A')
+
+print(f'A letra "A" aparece {aparecer} vezes')
+print(f'A letra "A" aparece pela primeira vez na posicao {primeira_vez}')
+print(f'A letra "A" aparece pela ultima vez na posicao {ultima_vez}')
