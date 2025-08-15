0d0cc0e5ade430b088770d13ceaded50e612cab5
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-14 22:16:18 -0300

    verificando primeiro nome

--- exercicios/ex024.py
+++ exercicios/ex024.py
@@ -1,1 +1,5 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela comeca ou nao com nome "Santo"
+cidade = str(input('Digite a cidade em que voce nasceu: ')).strip().split()
+primeiro_nome = cidade[0].lower().capitalize()
+print(f'A cidade que voce nasceu comeca com Santo? ')
+print(f'(Sim = 0), (Não = -1):  {primeiro_nome.find('Santo')}')
