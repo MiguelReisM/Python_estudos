02714c5e2b8d524d633ebce8db63cbb5210fc2ad
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-14 21:21:16 -0300

    exercicios novos

---
+++ exercicios/ex022.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia um nome completo de uma pessoa e mostre:
+# O nome com todas as letras maiusculas
+# O nome com todas minusculas
+# Quatas letras ao todo (sem considerar espacos)
+# Quantas letras tem o primeiro nome.
---
+++ exercicios/ex027.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia o nome de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente.
+# ex: Ana Maria de Souza
+# Primeiro = Ana
+# Ultimo = Souza
