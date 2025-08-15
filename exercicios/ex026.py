943628b9040e1c8add05eadd40c3977be9eb34ab
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-14 22:50:41 -0300

    ajustes

--- exercicios/ex026.py
+++ exercicios/ex026.py
@@ -8,5 +8,5 @@ primeira_vez = frase.find('A')
 ultima_vez = frase.rfind('A')
 
 print(f'A letra "A" aparece {aparecer} vezes')
-print(f'A letra "A" aparece pela primeira vez na posicao {primeira_vez}')
-print(f'A letra "A" aparece pela ultima vez na posicao {ultima_vez}')
+print(f'A letra "A" aparece pela primeira vez na posicao de index {primeira_vez}')
+print(f'A letra "A" aparece pela ultima vez na posicao de index {ultima_vez}')
