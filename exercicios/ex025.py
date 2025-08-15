4ba86ac5df8a4efd600048c32594a35fc07f45b2
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-14 22:38:51 -0300

    verificar nome

--- exercicios/ex025.py
+++ exercicios/ex025.py
@@ -1,1 +1,6 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
+nome = str(input('Digite seu nome completo: ')).lower().title().strip().split()
+silva = ('Silva') in nome
+print('No seu nome encontramos "Silva" ?')
+print(silva)
+
