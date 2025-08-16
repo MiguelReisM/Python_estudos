b5dfeb45c86bd02192fc142a2696817dccf47321
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-16 14:57:49 -0300

    melhorias

--- exercicios/ex30.py
+++ exercicios/ex30.py
@@ -1,7 +1,7 @@
 #Crie um programa que leia um numero inteiro qualuqer e mostre se ele é PAR ou IMPAR
 numero = int(input('Digite um numero:'))
-par_impar = numero % 2
-if par_impar == 1:
+
+if (numero % 2) == 1:
     print(f'O numero {numero} é impar!')
 else:
         print(f'O numero {numero} é par!')
