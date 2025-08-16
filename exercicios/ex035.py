5bd0cfad9e0d64e97ae8d91e38c180ac4e1714be
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-16 15:35:35 -0300

    melhoria

--- exercicios/ex035.py
+++ exercicios/ex035.py
@@ -1,15 +1,9 @@
 #Crie um programa que leia o comprimento de 3 retas e diga ao ususario se elas podem ou nao formar um triangulo
-reta1 = int(input('Digite o valor da primeira reta: '))
-reta2 = int(input('Digite o valor da segunda reta: '))
-reta3 = int(input('Digite o valor da terceira reta: '))
+reta1 = float(input('Digite o valor da primeira reta: '))
+reta2 = float(input('Digite o valor da segunda reta: '))
+reta3 = float(input('Digite o valor da terceira reta: '))
 
-if reta1 + reta2 > reta3:
-    if reta1 + reta3 > reta2:
-        if reta2 + reta3 > reta1:
-            print('É possivel formar um triangulo!')
-        else:
-            print('Nao é possivel formar um triangulo')
-    else:
-        print('Nao é possivel formar um triangulo')
+if reta1 + reta2 > reta3 and reta1 + reta3 > reta2 and reta2 + reta3 > reta1:
+    print('É possivel formar um triangulo!')
 else:
     print('Nao é possivel formar um triangulo')
