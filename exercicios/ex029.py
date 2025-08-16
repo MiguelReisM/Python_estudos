7582436ff75f86025395ce382b284666ce4c8b7c
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-16 14:55:32 -0300

    melhorias

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -1,9 +1,8 @@
 #Crie um programa que leia a velocidade de um carro.
 # Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado
 # A multa vai custar R$7,00 por cada Km acima do limite
-velocida = int(input('Digite a velocidade do carro ao passar no radar: '))
-multa = velocida - 80
+velocida = float(input('Digite a velocidade do carro ao passar no radar: '))
 
 if velocida > 80:
-    multa = float(multa * 7)
+    multa = (velocida - 80) * 7
     print(f'Voce ultrapassou o limite de velocidade e será multado em R${multa:.2f}')
