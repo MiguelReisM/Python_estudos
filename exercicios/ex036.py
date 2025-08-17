0af863437301111bbd5dda74c3f66b1050d1be88
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-17 14:29:58 -0300

    ajuste

--- exercicios/ex036.py
+++ exercicios/ex036.py
@@ -10,7 +10,7 @@ print('=-' * 20)
 
 prestcao = valor_casa / (anos_pagamento * 12)
 
-if prestcao < (salario * 0.30):
+if prestcao <= (salario * 0.30):
     print(f'O emprestimo foi aprovado! \nO imovel de R${valor_casa:.2f}, será pago em {(anos_pagamento * 12)} meses, \ne o valor das prestcoes será de R${prestcao:.2f}.')
 else:
     print(f'Infelizmente a prestacao de R${prestcao:.2f} ultrapaca 30% do seu salario de R${salario}, e o imprestimo foi negado!')
