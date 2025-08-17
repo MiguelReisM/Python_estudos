c8b93353ddea08847f2f5b2f1b12f25e15b4617d
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-17 15:26:04 -0300

    ajustes

--- exercicios/ex043.py
+++ exercicios/ex043.py
@@ -5,17 +5,17 @@
 # 30 até 40: Obesidade
 # Acima dos 40: Obsidade morbida
 tipos_imc = ['Abaixo do peso', 'Peso ideal', 'Sobrepeso', 'Obesidade', 'Obsidade morbida']
-altura = float(input('Digite sua altura em cm: '))
+altura = float(input('Digite sua altura em M: '))
 peso = float(input('Digite seu peso em Kg: '))
-imc = peso / (altura / 100 ) ** 2
+imc = peso / (altura / 100) ** 2
 
 if imc < 18.5:
     print(f'O seu IMC de {imc:.2f} se configura em {tipos_imc[0]}')
 elif imc >= 18.5 and imc < 25:
     print(f'O seu IMC de {imc:.2f} se configura em {tipos_imc[1]}')
-elif imc >= 25 and imc < 30:
+elif imc >= 25 and imc <= 30:
     print(f'O seu IMC de {imc:.2f} se configura em {tipos_imc[2]}')
-elif imc >= 30 and imc < 40:
+elif imc > 30 and imc <= 40:
     print(f'O seu IMC de {imc:.2f} se configura em {tipos_imc[3]}')
 else:
     print(f'O seu IMC de {imc:.2f} se configura em {tipos_imc[4]}')
