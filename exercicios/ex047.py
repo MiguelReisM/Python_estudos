a4caec33f3fe5cfe2d7e557989adaede6ba3e4f9
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-18 19:59:47 -0300

    imprimindo numeros pares de 1 a 50

--- exercicios/ex047.py
+++ exercicios/ex047.py
@@ -1,5 +1,5 @@
 #Crie um programa que mostre na tela todos os numeros pares que estao no intervalo entre 1 e 50!
 print('Imprimindo numeros pares de 1 a 50: ')
 
-for n in range(0, 50+1, 2):
+for n in range(2, 50+1, 2):
     print(f'{n}')
