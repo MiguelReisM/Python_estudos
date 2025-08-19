8c706dfd679f659ab9d4991f2c4bf0bdc960ae6a
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-19 12:41:01 -0300

    ajuste visual

--- exercicios/ex055.py
+++ exercicios/ex055.py
@@ -1,6 +1,6 @@
 #Crie um programa que leia o peso de 5 pessoas. No final mostre qual foi o maior e o menor peso lidos.
 menor = 0
-maior =0
+maior = 0
 
 for p in range(1, 6):
     peso = float(input((f'Por gentileza a {p}° pessoa informe seu peso:')))
@@ -10,7 +10,6 @@ for p in range(1, 6):
     else:
         if peso < menor:
             menor = peso
-        else:
-            if peso > maior:
-                maior = peso
+        if peso > maior:
+            maior = peso
 print(f'O menor peso foi {menor}Kg e o maior {maior}Kg')
