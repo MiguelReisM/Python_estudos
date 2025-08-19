e00162c3bb8148fa46338247474cc53341a69295
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-18 21:25:44 -0300

    formando palindormos (melhorias)

--- exercicios/ex053.py
+++ exercicios/ex053.py
@@ -1,16 +1,13 @@
 #Crie um programa que leia uma frase qualquer e diga se ela é um palindromo, desconsiderando os espacos
 print('Analisador de palindromos!')
-frase = input('Digite sua frase: ').lower()
-frase = frase.replace(' ','')
-frase = (frase)
+frase = input('Digite sua frase: ').lower().replace(' ','')
 c = len(frase)
 formou = True
 
-for n in range(0, c):
-    if frase[n] != frase[c-1]:
+for n in range(c // 2):
+    if frase[n] != frase[c - n - 1]:
         formou = False
         break
-    c -= 1
 
 if formou == True:
     print('Formou um palindromo!')
