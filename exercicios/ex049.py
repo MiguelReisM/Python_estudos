4e6641893ac1b2dae51006eae93464ea22459f3f
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-19 09:55:09 -0300

    melhorias

--- exercicios/ex049.py
+++ exercicios/ex049.py
@@ -1,6 +1,5 @@
 #Crie um programa que leia um numero inteiro qualquer e mostre na tela sua tabuada usando um laco for
 num = int(input('Digite um numero para ver sua tabuada: '))
-resultado = 0
+
 for n in range(1, 11):
-    resultado = n * num
-    print(f'({n}) X ({num}) = {resultado}')
+    print(f'({n}) X ({num}) = {n * num}')
