4d93be502f196ba08a924e7d90113ad8f62e23e0
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-19 12:19:00 -0300

    calculo de maioridade

--- exercicios/ex054.py
+++ exercicios/ex054.py
@@ -1,1 +1,13 @@
 #Crie um programa que leia o ano de nascimento de 7 pessoas. No final mostre quantas pessoas ainda nao atingiram a maioridade e quantas ja atingiram
+from datetime import date
+
+maior = 0
+menor = 0
+
+for p in range(1, 8):
+    ano = int(input((f'Por gentileza a {p}° pessoa informe o seu ano de nascimento: ')))
+    if  date.today().year - ano >= 18:
+        maior += 1
+    else:
+        menor += 1
+print(f'{menor} pessoa(as) nao atingiram a maioridade, e {maior} pessoa(as) já atingiram a maioridade')
--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -2,3 +2,10 @@
 # A media de idade do grupo
 # Qual o nome do homem mais velho
 # Quantas mulheres tem menos de 20 anos
+for p in range(1, 5):
+    print(f'Por gentileza a {p}° pessoa informe os dados a seguir:')
+    nome = input('Digite seu nome: ').strip()
+    idade = int(input('Digite sua idade: '))
+    sexo = input('Digite m se voce for mulher e h se for homem: ').lower().replace('','')
+
+
