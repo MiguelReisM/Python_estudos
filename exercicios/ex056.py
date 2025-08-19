133cd9c8d921bb218139e8c76634f22eee02c70f
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-19 13:04:16 -0300

    loops com condicionais e aninhamento

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -2,10 +2,26 @@
 # A media de idade do grupo
 # Qual o nome do homem mais velho
 # Quantas mulheres tem menos de 20 anos
+from datetime import date
+
+nome_h = 'nenhum'
+idade_h = 0
+idade_m = 0
+total = 0
+
 for p in range(1, 5):
     print(f'Por gentileza a {p}° pessoa informe os dados a seguir:')
-    nome = input('Digite seu nome: ').strip()
+    nome = input('Digite seu nome: ').strip().title()
     idade = int(input('Digite sua idade: '))
-    sexo = input('Digite m se voce for mulher e h se for homem: ').lower().replace('','')
+    sexo = input('Sexo [M/F]: ').lower().replace('','')
+    total += idade
+    if sexo == 'm' and idade_h < idade:
+        idade_h = idade
+        nome_h = nome
+    elif sexo == 'f' and idade < 20:
+        idade_m += 1
 
+print(f'A media de idade desse grupo de pessoas ficou: {total / 4}')
+print(f'O nome do homem mais velho é {nome_h} e a idade dele é {idade_h}')
+print(f'Tem {idade_m} mulheres menores de 20 anos')
 
