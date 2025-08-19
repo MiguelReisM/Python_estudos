5e5828b1ded6cd320fef631860e49c850a599130
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-19 10:48:42 -0300

    melhorias visuais

--- exercicios/ex052.py
+++ exercicios/ex052.py
@@ -1,16 +1,16 @@
 #Crie um programa que leia um numero inteiro e diga se ele é ou nao um numero primo
 num = int(input("Digite um número inteiro: "))
+cont = 0
 
-if num <= 1:
-    print(f"{num} não é um número primo.")
-else:
-    primo = True
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            primo = False
-            break
-
-    if primo:
-        print(f"{num} é um número primo.")
+for n in range(1, num + 1):
+    if num % n == 0:
+        print(f'\033[32m',end=' ')
+        cont += 1
     else:
-        print(f"{num} não é um número primo.")
+        print(f'\033[31m',end=' ')
+    print(f'{n}',end='')
+
+if cont == 2:
+    print(f'\n\033[mO numero {num} é primo!')
+else:
+    print(f'\n\033[mO numero {num} nao é primo!')
