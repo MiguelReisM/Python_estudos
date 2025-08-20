0a574e0bc747d902b44bbe5c2d218a903b060b95
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-19 22:06:06 -0300

    mais exercicios

---
+++ exercicios/ex063.py
@@ -0,0 +1,2 @@
+#Crie um programa que leia um numero n inteiro qualuqer e mostre na tela os n primeiros elementos de uma sequencia de fibonacci
+# 0 - 1 - 1 - 2 - 3 - 5 - 8
---
+++ exercicios/ex065.py
@@ -0,0 +1,2 @@
+#Crie um programa que leia varios numeros inteiros pelo teclado. No final da execucao, mostre a media entre todos os valores e qual foi o maior e o menor valores lidos.
+# O programa deve perguntar ao ususario se ele quer ou nao continuar a digitar valores
