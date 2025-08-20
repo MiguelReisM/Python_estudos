077f862b30381608b3beadbbefc0757066479dbd
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-20 20:07:07 -0300

    usando break e while true

---
+++ exercicios/ex066.py
@@ -0,0 +1,10 @@
+#Crie um programa que leia varios numeros inteiros pelo teclado. O programa so vai parar quando o usuario digitar o valor 999, que é a condicao de parada. No final, mostre quantos numeros foram digitados e qual foi a soma entre eles, descondire o flag
+somatoria = cont = 0
+
+while True:
+    num = int(input('Digite um numero inteiro: '))
+    if num == 999:
+        break
+    somatoria += num
+    cont += 1
+print(f'Foram digitados {cont} numeros e a somatoria deles deu: {somatoria}')
