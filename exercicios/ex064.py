67d7879e9e8c89dfb39299068ca0f77f93c9b059
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-20 17:21:53 -0300

    somatoria com condicional de parada

--- exercicios/ex064.py
+++ exercicios/ex064.py
@@ -1,1 +1,8 @@
 #Crie um programa que leia varios numeros inteiros pelo teclado. O programa só vai parar quando o usuario digitar o valor 999, que é a condicao de parada. No final mostre quantos numeros foram digitados e qual foi a soma entre eles, desconsiderando o 999
+num = cont = soma = 0
+
+while num != 999:
+    num = int(input('Digite um numero inteiro: '))
+    cont += 1
+    soma += num
+print(f'Voce digitou {cont-1} numero(os) e a somatoria deles é {soma-999}')
