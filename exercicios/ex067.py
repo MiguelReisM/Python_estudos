72ef8c067401c2e9b346259d44cb295c77c30e7a
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-20 20:14:56 -0300

    tabuada com break

---
+++ exercicios/ex067.py
@@ -0,0 +1,12 @@
+#Crie um programa que mostre a tabuada de varios numeros, um de cada vez, para cada valor digitadopelo usuario. O programa sera interrompido quando for solicitado um numero negativo.
+while True:
+    print('=-'*20)
+    num = int(input('Digite um numero para ver sua tabuada, \ncaso deseje sair digite um numero negativo: '))
+    cont = 1
+    print('=-'*20)
+    if num < 0:
+        print('Programa encerrado!')
+        break
+    while cont <= 10:  
+        print(f'{num} X {cont} = {num * cont}')
+        cont += 1
