4926db14bebf364f4200c9b8b966c12cfd6b8697
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-20 17:47:18 -0300

    melhorias

--- exercicios/ex058.py
+++ exercicios/ex058.py
@@ -3,14 +3,14 @@
 from random import randint
 
 tentativa = 0
-num_c = 0
+num_c = randint(0, 10)
 num_h = 1
 
 while num_c != num_h:
-    num_c = randint(0, 10)
     num_h = int(input('Digite um numero entre 0 e 10: '))
     tentativa += 1
-    print(f'O computador jogou {num_c} e voce jogou {num_h}')
-    if num_c != num_h:
-        print('Voce perdeu, proxima tentativa')
-print(f'Parabens voce ganhou e usou {tentativa} tentativas')
+    if num_h > num_c:
+        print('Tente um menor!')
+    elif num_h < num_c:
+        print('Tente um maior!')
+print(f'Parabens voce ganhou!\nO numero erá {num_c} e voce usou {tentativa} tentativas')
