72a516a95249610a7b3088c4f4604607aa5fe62a
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-22 18:21:32 -0300

    conferindo abertura e fechamento de parenteses em uma expressao

--- exercicios/ex083.py
+++ exercicios/ex083.py
@@ -1,2 +1,12 @@
 #Crie um programa onde o usuario digite uma expressao qualquer que use parenteses. 
 # Seu aplicativo devera analisar se a expressao passada está com os parenteses abertos e fechados na ordem correta (use listas)
+lista_mat = []
+expressao = input('Digite sua expressao matematica: ').replace(' ','')
+
+parenteses_aberto = expressao.count('(')
+parenteses_fechado = expressao.count(')')
+
+if parenteses_aberto == parenteses_fechado:
+    print('Essa expressao é valida!')
+else:
+    print('Essa expressao é invalida!')
