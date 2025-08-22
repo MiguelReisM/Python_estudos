a754578f0b0adf37df5305a09675b96c22db9e33
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-22 20:15:28 -0300

    ajustes e correcoes

--- exercicios/ex080.py
+++ exercicios/ex080.py
@@ -1,46 +1,22 @@
 #Crie um programa onde o ususario possa digitar cinco valores numericos e cadastre-os em uma lista, ja na posicao correta de insercao (sem usar o sort())
 # No final, mostre a lista ordenada na tela
 lista_num = []
-maior = menor = 0
+
 for n in range(5):
     numero = int(input('Digite um numero: '))
-    if n == 0:
+    if n == 0 or numero > lista_num[-1]:
+        lista_num.append(numero)
+        print(f'O número {numero} foi adicionado ao final da lista!')
+    elif numero < lista_num[0]:
         lista_num.insert(0, numero)
-        print(f'Primeiro numero adicionado a lista com sucesso!')
-        maior = numero
-        menor = numero
-    elif n == 1:
-        if numero > maior:
-            lista_num.append(numero)
-            maior = numero
-        elif numero < menor:
-            lista_num.insert(0, numero)
-            menor = numero
-        else:
-            lista_num.append(numero)
-    elif n == 2:
-        if numero > maior:
-            lista_num.append(numero)
-        elif numero < menor:
-            lista_num.insert(0, numero)
-        else:
-            i = lista_num.index(numero)
-            lista_num.insert(i, numero)
-    elif n == 3:
-        if numero > maior:
-            lista_num.append(numero)
-        elif numero < menor:
-            lista_num.insert(0, numero)
-        else:
-            i = lista_num.index(numero)
-            lista_num.insert(i, numero)
+        print(f'O número {numero} foi adicionado no início da lista!')
     else:
-        if numero > maior:
-            lista_num.append(numero)
-        elif numero < menor:
-            lista_num.insert(0, numero)
-        else:
-            i = lista_num.index(numero)
-            lista_num.insert(i, numero)
+        posicao = 0
+        while posicao < len(lista_num):
+            if numero <= lista_num[posicao]:
+                lista_num.insert(posicao, numero)
+                print(f'O número {numero} foi adicionado na {posicao + 1}° posição da lista!')
+                break
+            posicao += 1
 
 print(lista_num)
