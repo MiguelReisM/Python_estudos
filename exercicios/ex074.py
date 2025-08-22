ecad9aca8ff273849eb16a18262cc8e7ce0a3299
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-21 21:02:05 -0300

    tuplas para numeros aleatorios

---
+++ exercicios/ex074.py
@@ -0,0 +1,12 @@
+#Crie um programa que vai gerar cinco numeros aleatorios e colocar em uma tupla
+# Depois disso, mostre a listagem de numeros gerados e tambem indique o menor e o maior valor que estao na tupla
+from random import randint
+
+numeros = []
+
+for n in range(5):
+    lista_n = numeros.append(randint(0,10))
+
+print(f'Números gerados: {numeros}')
+print(f'Maior número: {max(numeros)}')
+print(f'Menor número: {min(numeros)}')
