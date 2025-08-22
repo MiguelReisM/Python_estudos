e2b664d0d239e1100a176620a8ef69643f09463c
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-22 14:12:47 -0300

    melhorias

--- exercicios/ex078.py
+++ exercicios/ex078.py
@@ -1,9 +1,22 @@
 #Crie um programa que leia 5 valores numericos e guarde os em uma lista.
 # No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posicoes na lista
 lista_num = []
+
 for numeros in range(5):
-    numero = int(input(f'Digite o seu {numeros + 1}° numero: '))
-    lista_num.append(numero)
+    num = int(input(f'Digite o seu {numeros + 1}° numero: '))
+    lista_num.append(num)
+
 print(f'A sua lista é: {lista_num}')
-print(f'Sendo o maior valor {max(lista_num)} encontrado na {lista_num.index(max(lista_num))+ 1}° posicao')
-print(f'Sendo o menor valor {min(lista_num)} encontrado na {lista_num.index(min(lista_num))+ 1}° posicao')
+
+maior = max(lista_num)
+menor = min(lista_num)
+
+print(f'\nO maior valor dessa lista é: {maior}, que aparece na(as) posicao(oes): ', end='')
+for c, n in enumerate(lista_num):
+    if n == maior:
+        print(f'{c + 1}', end=' ')
+
+print(f'\nO menor valor dessa lista é: {menor}, que aparece na(as) posicao(oes): ', end='')
+for c, n in enumerate(lista_num):
+    if n == menor:
+        print(f'{c + 1}', end=' ')
