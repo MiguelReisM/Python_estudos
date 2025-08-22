07e6fed7a14a47923ea4fd13c9e58a4bfdc1f11f
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-21 21:17:32 -0300

    input de valores na tupla e analise de dados

--- exercicios/ex075.py
+++ exercicios/ex075.py
@@ -3,6 +3,13 @@
 # em que posicao foi digitado o primeiro valor 3
 # quais foram os numeros pares
 lista_num = []
+lista_par = []
 for c, n in enumerate(range(4)):
-    num = int(input(f'Digite o {c} valor: '))
+    num = int(input(f'Digite o {c+1} valor: '))
+    if num % 2 == 0:
+        lista_par.append(num)
     lista_num.append(num)
+print(f'Aqui esta a sua tupla: {lista_num}')
+print(f'O valor 9 apreceu {lista_num.count(9)} vezes')
+print(f'O valor 3 apreceu primeiro na {lista_num.index(3+1)} posicaos')
+print(f'Os valores pares foram {lista_par}')
