77718edf902a1cbe8f11af3ffe3e2bf95467605f
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-22 20:00:34 -0300

    melhorias

--- exercicios/ex079.py
+++ exercicios/ex079.py
@@ -1,33 +1,24 @@
 #Crie um programa onde o usuario possa digitar varios valores numericos e cadastre-os em uma lista. Caso o numero ja exista la dentro, ele nao sera adicionado
-# No final, serao exibidos todos os valores unicos digitados, em ordem crescente 
+# No final, serao exibidos todos os valores unicos digitados, em ordem crescente.
 lista_numerica = []
 contador = 0
 
 while True:
-    while contador == 0:
+    while True:
         numero = int(input('Digite um valor: '))
-        lista_numerica.append(numero)
-        break
-
-    while contador > 0:
-        numero = int(input('Digite um valor: '))
-        existe = lista_numerica.count(numero)
-        if existe > 0:
-            print('Esse numero ja foi inserido, tente outro!')
-        else:
+        if numero not in (lista_numerica):
             lista_numerica.append(numero)
             break
+        else:
+            print('Esse valor ja esta na lista, digite outro!')
 
     while True:
         opcao = ''
         opcao = (input('Voce deseja continuar? (S/N): ')).strip().lower()[0]
-        if 'n' in opcao: break
-        elif 's' in opcao: 
-            break
+        if opcao in 'sn': break
         else: print('Digite uma opcao valida')
-    if opcao == 'n': break
 
-    contador += 1
+    if opcao == 'n': break
 
 print('=-' * 20)
 print(sorted(lista_numerica))
