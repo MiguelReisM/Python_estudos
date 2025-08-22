3677caf9d8a929c05305ba15c8595fc47582fc64
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-22 18:03:13 -0300

    separacao de pares e impares em listas

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -2,3 +2,30 @@
 # Quantos numeros foram digitados
 # A lista de valores ordenado de forma decrescente
 # Se o valor 5 foi digitado e esta ou nao na lista
+lista_numerica = []
+lista_par = []
+lista_impar = []
+c = 0
+
+while True:
+    numero = int(input('Digite um numero: '))
+    lista_numerica.append(numero)
+    c += 1
+
+    if numero % 2 == 0:
+        lista_par.append(numero)
+    else:
+        lista_impar.append(numero)
+
+    while True:
+        opcao = ''
+        opcao = (input('Voce deseja continuar? (S/N): ')).strip().lower()[0]
+        if 'n' in opcao: break
+        elif 's' in opcao: 
+            break
+        else: print('Digite uma opcao valida')
+    if opcao == 'n': break
+
+print(f'\nForam inseridos {c} numeros na lista: {sorted(lista_numerica)}')
+print(f'Sendo esses os pares {sorted(lista_par)}')
+print(f'E esses os impares {sorted(lista_impar)}')
