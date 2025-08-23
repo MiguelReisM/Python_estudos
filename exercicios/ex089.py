d182c1df48fa5733ddd4dbe81b92337e3e27c459
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-23 17:50:11 -0300

    melhorias

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -1,46 +1,32 @@
 #Crie um programa que leia nome e duas notas de varios alunos e guarde tudo em uma lista composta.
 # No final mostre um boletim contendo a media de cada um e permita que o usuario possa mostrar as notas de cada aluno indevidualmente
 alunos = []
-medias = []
-cont = 0
 
 while True:
     nome = input('Nome: ').strip().title()
     nota1 = float(input('Nota 1: '))
     nota2 = float(input('Nota 2: '))
     media = (nota1 + nota2) / 2
-    medias.append([nota1, nota2])
-    alunos.append([cont, nome, media])
+    alunos.append([nome, [nota1, nota2], media])
 
-    while True:
-        opcao = ''
-        opcao = input('Quer continuar? (S/N): ').strip().lower()[0]
-        if opcao in 'n':
-            break
-        elif opcao == 's':
-            break
-        else:
-            print('Opcao invalida!')
+    opcao = input('Quer continuar? (S/N): ').strip().lower()
     if opcao == 'n':
         break
 
-    cont += 1
-
 print('-=' * 11)
-print('No. Nome      Media')
+print(f'{"No.":<4}{"Nome":<10}{"Media":>6}')
 print('-' * 22)
-for aluno in range(cont + 1):
-    print(f'{alunos[aluno][0]:<3}', end=' ')
-    print(f'{alunos[aluno][1]:<10}', end=' ')
-    print(f'{alunos[aluno][2]:<3}', end=' ')
-    print()
+
+for i, aluno in enumerate(alunos):
+    print(f'{i:<4}{aluno[0]:<10}{aluno[2]:>6.1f}')
 
 print('-' * 22)
+
 while True:
-    no = int(input('Quer mostrar as notas de qual aluno? (999 interoompe): '))
+    no = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
     if no == 999:
         break
-    elif no <= cont:
-        print(f'As notas da(o) {alunos[no][1]} sao: {medias[no]}')
+    elif 0 <= no < len(alunos):
+        print(f'As notas de {alunos[no][0]} sao: {alunos[no][1]}')
     else:
         print('Aluno nao cadastrado!')
