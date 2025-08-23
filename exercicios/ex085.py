75b33f3f5b8ba1baa944abd25be38cd8c0398e63
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-23 17:20:31 -0300

    melhorias

--- exercicios/ex085.py
+++ exercicios/ex085.py
@@ -1,17 +1,13 @@
 #Crie um programa onde o usuario possa digitar sete valores numericos e cadastre-os em uma lista unica que mantenha separdos os valores pares e impares.
 # No final, mostre os valores pares e impares em ordem crescente (uma lista com 2 listas internas)
-lista_valores = []
-par = []
-impar = []
+lista_valores = [[], []]
 
-for v in range(7):
-    valor = int(input((f'Digite o {v + 1} valor: ')))
+for c in range(1,8):
+    valor = int(input((f'Digite o {c} valor: ')))
     if valor % 2 == 0:
-        par.append(valor)
+        lista_valores[0].append(valor)
     else:
-        impar.append(valor)
-
-lista_valores = ([par, impar])
+        lista_valores[1].append(valor)
 
 print(f'Os valores pares digitados sao: {sorted(lista_valores[0])}')
 print(f'Os valores impares digitados sao {sorted(lista_valores[1])}')
