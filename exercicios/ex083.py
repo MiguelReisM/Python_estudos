f34a651ed13e9d3606a043233c7a57c79ea59efe
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-23 10:00:25 -0300

    ajustes

--- exercicios/ex083.py
+++ exercicios/ex083.py
@@ -1,6 +1,5 @@
-#Crie um programa onde o usuario digite uma expressao qualquer que use parenteses. 
-# Seu aplicativo devera analisar se a expressao passada está com os parenteses abertos e fechados na ordem correta (use listas)
-lista_mat = []
+#Crie um programa onde o usuario digite uma expressao qualquer que use parenteses.
+# Seu aplicativo devera analisar se a expressao passada está com os parenteses abertos e fechados na ordem correta
 expressao = input('Digite sua expressao matematica: ').replace(' ','')
 
 parenteses_aberto = expressao.count('(')
