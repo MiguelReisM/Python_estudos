b06ffe586be018890f956e6fe2346b4c636e97e1
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-24 17:44:04 -0300

    cadastro trabalhador

--- exercicios/ex092.py
+++ exercicios/ex092.py
@@ -1,2 +1,28 @@
 #Crie um programa que leie o nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionario se por acaso a CTPS,
 # for diferente de ZERO. o dicionario recebra tambem o ano de contratacao e o salrio. Calcule e acrescente, alem da idade, com quantos anos a pessoa vai se aposentar (=35 anos de contratacao)
+from datetime import date
+
+pessoas = {'nome' : 'Ana',
+           'idade' : 0,
+           'ctps' : 0,
+           'ano_contrat' : 0,
+           'salario' : 0.00,
+           'aposentadoria' : 0
+            }
+
+pessoas['nome'] = input('Digite nome: ').strip().title()
+ano_nasc = int(input('Digite o ano que nasceu: '))
+pessoas['idade'] = (date.today().year - ano_nasc)
+pessoas['ctps'] = int(input('Carteira de trabalho (0 nao tem): '))
+if pessoas['ctps'] != 0:
+    pessoas['ano_contrat'] = int(input('Ano contratacao: '))
+    pessoas['salario'] = float(input('Digite o salario: R$'))
+    pessoas['aposentadoria'] = ((pessoas['ano_contrat'] - ano_nasc) + 35)
+
+print(f'\nNome tem o valor: {pessoas["nome"]}')
+print(f'Idade tem o valor: {pessoas["idade"]} anos')
+print(f'CTPS tem o valor: {pessoas["ctps"]}')
+if pessoas['ctps'] != 0:
+    print(f'Ano contratacao tem o valor: {pessoas["ano_contrat"]}')
+    print(f'Salario tem o valor: R${pessoas["salario"]:.2f}')
+    print(f'Aposentadoria tem o valor: {pessoas["aposentadoria"]} anos')
--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -1,3 +1,4 @@
 #Crie um programa que gerencie o aproveitamento de varios jogadores de futebol. O programa vai ler o nome dos jogadores e quantas partidas eles jogaram.
 # Depois vai ler quantidade de gols feitos em cada partida. No final, tudo isso sera guardado em um dicionario, incluindo o total de gols feito durante o campeonato
 # Inclua um sistema de visualizacao de detalhes do aproveitamento de cada jogador
+
