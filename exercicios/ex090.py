72619b98b79dbd787653aef4af74cf939134d908
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-24 20:37:42 -0300

    melhorias e ajustes

--- exercicios/ex090.py
+++ exercicios/ex090.py
@@ -1,12 +1,15 @@
-#Crie um programa que leia o nome e media de um aluno, guardando tambem a situacao em um dicionario. No final mostre o conteudo da estrutura na tela
-aluno = {'nome' : '',
-         'media' : 0.0}
+#Crie um programa que leia o nome e media de um aluno, guardando tambem a situacao em um dicionario. (media >= 6 aprovado)
+# No final mostre o conteudo da estrutura na tela
+aluno = {}
 
 aluno['nome'] = input('Digite o nome: ')
 aluno['media'] = float(input('Digite a media: '))
-print(f'O nome é {aluno["nome"]}')
-print(f'A média é {aluno["media"]}')
+
 if aluno['media'] >= 6:
-    print('Situacao aprovado!')
+    aluno["situacao"] = 'Aprovado'
 else:
-    print('Situacao reprovado!')
+    aluno["situacao"] = 'Reprovado'
+
+print('-' * 30)
+for k, v in aluno.items():
+    print(f'{k.title()}: {v}')
