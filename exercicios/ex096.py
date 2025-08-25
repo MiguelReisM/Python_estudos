1c4d08ec2211dc1104f042dcb133a113f7a5bf9c
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-25 16:32:40 -0300

    ajustes

--- exercicios/ex096.py
+++ exercicios/ex096.py
@@ -1,9 +1,9 @@
 #Crie um programa que tenha uma funcao chamada area(). que receba as dimensoes de um terreno retangular (largura e comprimento) e mostre a area do retangulo
-def area(larg, alt):
-    area = larg * alt
+def area(larg, comp):
+    area = larg * comp
     print(f'A area desse terreno é {area:.1f} metros quadrados')
 
 
 larg = float(input('Digite a largura do terreno (M): '))
-alt = float(input('Digite a altura do terreno (M): '))
-area(larg, alt)
+comp = float(input('Digite o comprimento do terreno (M): '))
+area(larg, comp)
--- exercicios/ex098.py
+++ exercicios/ex098.py
@@ -4,36 +4,32 @@
 # de 10 ate 0 de 2 em 2
 # uma contagem personalizada
 def contador(inicio, fim, passo):
-    if inicio < fim and passo > 0 or inicio > fim and passo < 0:
-        print(f'A contagem de {inicio} até {fim} de {passo} em {passo} é: ')
-        for c in range(inicio, fim + 1, passo):
-            print(f'{c}', end=' ')
-    elif inicio > fim and passo > 0 or inicio < fim and passo < 0:
-        print(f'A contagem de {inicio} até {fim} de {passo} em {passo} é: ')
-        for c in range(inicio, fim - 1, - (passo)):
-            print(c, end=' ')
-    elif inicio < fim and passo == 0:
-        print(f'A contagem de {inicio} até {fim} de {passo} em {passo} nao existe, alterando para 1 em 1: ')
-        for c in range(inicio, fim + 1, 1):
-            print(c, end=' ')
+    if passo < 0:
+        passo *= -1
+    if passo == 0:
+        print(f'A contagem de {inicio} até {fim} de {passo} em {passo} nao existe, alterando para 1 em 1:', end=' ')
+        passo = 1
+
+    print(f'\nA contagem de {inicio} até {fim} de {passo} em {passo}:', end=' ')
+
+    if inicio < fim:
+        c = inicio
+        while c <= fim:
+            print(f'{c} ', end='')
+            c += passo
     else:
-        print(f'A contagem de {inicio} até {fim} de {passo} em {passo} nao existe, alterando para 1 em 1: ')
-        for c in range(inicio, fim - 1, - 1):
-            print(c, end=' ')
-    print(f'\n{"-" * 40}')
+        c = inicio
+        while c >= fim:
+            print(f'{c} ', end='')
+            c -= passo
 
 
-inicio_a = 1
-fim_a = 10
-passo_a = 1
-contador(inicio_a, fim_a, passo_a)
+contador(1, 10, 1)
+contador(10, 0, 2)
 
-inicio_b = 10
-fim_b = 0
-passo_b = 2
-contador(inicio_b, fim_b, passo_b)
-
-inicio_c = int(input('Inicio: '))
-fim_c = int(input('Fim: '))
-passo_c = int(input('Passo: '))
-contador(inicio_c, fim_c, passo_c)
+print(f'\n{"-=" * 20}')
+print('Agora é sua vez de personalizar a contagem!')
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(inicio, fim, passo)
