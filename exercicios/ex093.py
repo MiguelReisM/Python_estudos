ccc61b10d3d46b12ebaadfe126f1588951b85b50
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-24 21:05:15 -0300

    ajustes

--- exercicios/ex093.py
+++ exercicios/ex093.py
@@ -2,22 +2,20 @@
 # Depois vai ler quantidade de gols feitos em cada partida. No final, tudo isso sera guardado em um dicionario, incluindo o total de gols feito durante o campeonato
 somatoria = 0
 gol_partida = []
-jogador = {'nome' : '',
-           'partidas' : 0,
-           'gols' : [],
-           'total' : 0
-           }
+jogador = {}
 
 jogador['nome'] = input('Nome: ').strip().title()
 jogador['partidas'] = int(input('Quantas partidas ele jogou? '))
+
 for p in range(jogador['partidas']):
-    partida = int(input(f'Quantos gols ele fez na {p} partida: '))
+    partida = int(input(f'Quantos gols ele fez na {p+1}ª partida: '))
     gol_partida.append(partida)
     somatoria += partida
+
 jogador['gols'] = gol_partida[:]
 jogador['total'] = somatoria
 
-print(f'\nO jogador {jogador['nome']}, jogou {jogador['partidas']} partidas')
+print(f'\nO jogador {jogador["nome"]} jogou {jogador["partidas"]} partidas.')
 for c in range(jogador['partidas']):
-    print(f' => Na partida {c}, fez: {gol_partida[c]} gol(s)')
-print(f'Foi um total de {jogador['total']} gol(s)')
+    print(f' => Na partida {c+1}, fez {gol_partida[c]} gol(s)')
+print(f'Foi um total de {jogador["total"]} gol(s)')
