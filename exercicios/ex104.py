54a88bc96a0a2838dadd65f45095b4b441e2afe0
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-27 21:28:00 -0300

    Adiciona exercício ex104.py

---
+++ exercicios/ex104.py
@@ -0,0 +1,12 @@
+#Crie um programa que tenha a funcao leiaint() que vai funcionar semelhante a funcao input() do Python, só que fazendo validacao para aceitar apenas valores numerico
+# ex: n = leiaInt('Digite um n)
+def leiaInt(msg):
+    while True:
+        n = input(msg)
+        if n.strip().lstrip('-').isnumeric():
+            return int(n)
+        else:
+            print('\033[31mErro! Digite um número inteiro válido.\033[m')
+
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
