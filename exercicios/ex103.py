d407bedd23bd8b40fb4bef141510f68e06bcabe8
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-27 21:27:59 -0300

    validando entrada de dados em python

--- exercicios/ex103.py
+++ exercicios/ex103.py
@@ -1,1 +1,17 @@
-#
+#Crie um programa que tenha uma funcao cahamada ficha(), que receba dois parametros: o nome de um jogador e quantos gols ele marcou.
+# O programa devera ser capaz de mostrar a ficha do jogador, mesmo que algum dado nao tenha sido informado corretamente
+def ficha(nome='', gols=''):
+
+    if nome.strip() == '':
+        nome = 'desconhecido'
+    if str(gols).isnumeric():
+        gols = int(gols)
+    else:
+        gols = 0
+
+    return f'O jogador {nome} fez {gols} gol(s) no campeonato.'
+
+
+n = input('Digite o nome do jogador: ').strip().title()
+g = input('Digite quantos gols ele fez: ')
+print(ficha(n, g))
