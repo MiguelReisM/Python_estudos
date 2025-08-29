a332a63e5fa564fb4e92b384d3d54382cdd7d059
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-28 21:04:53 -0300

    formatando moedas com modulos

--- exercicios/ex108.py
+++ exercicios/ex108.py
@@ -1,8 +1,8 @@
 #Adpte o codigo do desafio 107 criando uma funcao adicional chamada moeda() que consiga mostrar os valores como valor monetario formatado
 from modulos_pacotes.moeda import moeda
 
-preco = float(input('Digite o preco: R$ '))
-print(f'A metade de R${preco} é R${moeda.metade(preco)}')
-print(f'O dobro de R${preco} é R${moeda.dobro(preco)}')
-print(f'O valor de R${preco} com 10% de aumento é R${moeda.aumentar(preco)}')
-print(f'O valor de R${preco} com 13% de desconto é R${moeda.diminuir(preco)}')
+preco = float(input('Digite o preço: R$ '))
+print(f'A metade de {moeda.moeda(preco)} é {moeda.moeda(moeda.metade(preco))}')
+print(f'O dobro de {moeda.moeda(preco)} é {moeda.moeda(moeda.dobro(preco))}')
+print(f'O valor de {moeda.moeda(preco)} com 10% de aumento é {moeda.moeda(moeda.aumentar(preco))}')
+print(f'O valor de {moeda.moeda(preco)} com 13% de desconto é {moeda.moeda(moeda.diminuir(preco))}')
--- exercicios/ex109.py
+++ exercicios/ex109.py
@@ -1,8 +1,8 @@
 #Modifique as funcoes que foram criadas no desafio 107 para que elas aceitem um parametro a mais, informando se o valor vai ser formatado pela funcao moeda(), desevolvida no desafio 108
 from modulos_pacotes.moeda import moeda
 
-preco = float(input('Digite o preco: R$ '))
-print(f'A metade de R${preco} é R${moeda.metade(preco)}')
-print(f'O dobro de R${preco} é R${moeda.dobro(preco)}')
-print(f'O valor de R${preco} com 10% de aumento é R${moeda.aumentar(preco)}')
-print(f'O valor de R${preco} com 13% de desconto é R${moeda.diminuir(preco)}')
+preco = float(input('Digite o preço: R$ '))
+print(f'A metade de {moeda.moeda(preco)} é {moeda.moeda(moeda.metade(preco))}')
+print(f'O dobro de {moeda.moeda(preco)} é {moeda.moeda(moeda.dobro(preco))}')
+print(f'O valor de {moeda.moeda(preco)} com 10% de aumento é {moeda.moeda(moeda.aumentar(preco))}')
+print(f'O valor de {moeda.moeda(preco)} com 13% de desconto é {moeda.moeda(moeda.diminuir(preco))}')
