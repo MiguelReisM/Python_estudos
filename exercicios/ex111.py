7528affdfded0df9e5c02b39e58a9ca5cc6b3ca8
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-28 21:56:31 -0300

    organizando modulos e pacotes

---
+++ exercicios/ex111.py
@@ -0,0 +1,6 @@
+#Crie um pacote chamado modulos_pacotes que tenha dois modulos internos chamados moeda e dado
+#Transfira todas as funcoes utilizadas nos desafios 107, 108, 109 e 110 para o primeiro pacote e mantenha tudo funcionando
+from modulos_pacotes.moeda import moeda                 #107
+from modulos_pacotes.moeda import moeda_formatada       #108
+from modulos_pacotes.moeda import moeda_formt_show      #109
+from modulos_pacotes.moeda import moeda_resumo          #110
